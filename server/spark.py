ccc52fe6aaf42e516826410e489f2a34cbe73443
Patrick Chen <patrickchen1994@gmail.com>
2017-06-21 22:48:45 +0800

    Add server

---
+++ server/spark.py
@@ -0,0 +1,32 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import HiveContext
+import requests
+import json
+import pyrebase
+
+from config import config
+
+class SparkHiveExample:
+
+    def __init__(self):
+        ## initialize spark session
+        self.spark = SparkSession.builder.appName("Spark Hive example").enableHiveSupport().getOrCreate()
+
+        firebase = pyrebase.initialize_app(config)
+        self.db = firebase.database()
+
+    def run(self):
+        ## download with opendata API
+        url = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=55ec6d6e-dc5c-4268-a725-d04cc262172b"
+        data = requests.get(url)
+
+        result = data.json()['result']['results']
+        self.db.child('metro').set(result)
+
+
+
+
+if __name__ == "__main__":
+    EXAMPLE = SparkHiveExample()
+    EXAMPLE.run()
+
